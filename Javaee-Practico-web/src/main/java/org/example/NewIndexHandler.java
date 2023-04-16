a74eb10c50cf198cae3b7bdde69a53e14f50d65b
Damian Dalto <ddalto@geocom.com.uy>
2023-04-16 18:00:02 -0300

    se actualiza look de la pagina de inicio

---
+++ Javaee-Practico-web/src/main/java/org/example/NewIndexHandler.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import org.example.model.Empresa;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "newIndexHandler")
+@ViewScoped
+public class NewIndexHandler implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String razonSocial;
+    private String nombrePublico;
+    private String direccion;
+    private String buscar;
+    private long nroEmpresa;
+    private List<Empresa> empresas = new ArrayList<>();
+
+    @EJB
+    EmprLocal empresaBean;
+
+    public NewIndexHandler(){}
+
+    public void agregarEmpresa(){
+        empresaBean.agregar(razonSocial, nombrePublico, direccion);
+    }
+
+    public void listarEmpresas(){
+        empresas = empresaBean.getEmpresas("Todos los campos", buscar);
+    }
+
+    public void borrarEmpresa(){
+        empresaBean.eliminarEmpresa(nroEmpresa);
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getNombrePublico() {
+        return nombrePublico;
+    }
+
+    public void setNombrePublico(String nombrePublico) {
+        this.nombrePublico = nombrePublico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getBuscar() {
+        return buscar;
+    }
+
+    public void setBuscar(String buscar) {
+        this.buscar = buscar;
+    }
+
+    public long getNroEmpresa() {
+        return nroEmpresa;
+    }
+
+    public void setNroEmpresa(long nroEmpresa) {
+        this.nroEmpresa = nroEmpresa;
+    }
+
+    public List<Empresa> getEmpresas() {
+        return empresas;
+    }
+
+    public void setEmpresas(List<Empresa> empresas) {
+        this.empresas = empresas;
+    }
+}
